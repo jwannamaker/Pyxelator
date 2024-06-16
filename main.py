429e43d484ebaeb81d4bec4b12d9c2f535e0bbfb
Johnny Wannamaker <j.wannamaker19@gmail.com>
2024-06-15 18:11:45 -0700

    Base main.py setup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QFileDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,16 +8,6 @@ class MainWindow(QMainWindow):
     def initUI(self):
         self.setWindowTitle('Pyxelator')
         
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QGridLayout(central_widget)
-
-        # Add viewports (placeholders for now)
-        for i in range(2):
-            for j in range(3):
-                viewport = QWidget()
-                layout.addWidget(viewport, i, j)
-
         self.show()
         
 if __name__ == '__main__':
